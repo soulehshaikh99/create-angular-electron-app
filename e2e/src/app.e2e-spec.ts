42987af1af235967eb750a614ee5f666cd8713d9
Souleh Shaikh <soulehshaikh99@gmail.com>
2019-09-18 02:33:47 +0530

    :chart_with_upwards_trend: END-To-END Testing Directory

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to create-angular-electron-app!');
+    expect(page.getTitleText()).toEqual('create-angular-electron-app app is running!');
   });
 
   afterEach(async () => {
