8c1b30e52e89b0cc3537e55c557e4101092324da
Souleh Shaikh <soulehshaikh99@gmail.com>
2019-09-18 02:35:33 +0530

    :page_with_curl: Source Directory

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,10 +26,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('create-angular-electron-app');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to create-angular-electron-app!');
+    expect(compiled.querySelector('.content span').textContent).toContain('create-angular-electron-app app is running!');
   });
 });
